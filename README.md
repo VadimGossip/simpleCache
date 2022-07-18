b14637d32bf6a54a9e9a1ae9dd85b934ba7fa71d
Vadim Voronin <vadvoronin@gmail.com>
2022-07-18 11:26:21 +0300

    code first look v1

---
+++ README.md
@@ -0,0 +1,2 @@
+Go Cache helps you to made simple cache based on map with Set\Get\Delete functions.
+Example
---
+++ cache.go
@@ -0,0 +1,33 @@
+package main
+
+import "fmt"
+
+type Cashier interface {
+	Set(key string, value interface{})
+	Get(key string) (interface{}, error)
+	Delete(key string)
+}
+
+type Cashe struct {
+	storage map[string]interface{}
+	Cashier
+}
+
+func NewCache() *Cashe {
+	return &Cashe{storage: make(map[string]interface{})}
+}
+
+func (c *Cashe) Set(key string, value interface{}) {
+	c.storage[key] = value
+}
+
+func (c *Cashe) Get(key string) (interface{}, error) {
+	if val, ok := c.storage[key]; ok {
+		return val, nil
+	}
+	return nil, fmt.Errorf("no data found")
+}
+
+func (c *Cashe) Delete(key string) {
+	delete(c.storage, key)
+}
