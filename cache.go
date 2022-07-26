4022edd1ee7b753ec5c879808654c90fd72703c7
Vadim Voronin <vadvoronin@gmail.com>
2022-07-26 22:07:00 +0300

    added lock of Cache and ttl for values

--- cache.go
+++ cache.go
@@ -1,33 +1,62 @@
 package simpleCache
 
-import "fmt"
+import (
+	"fmt"
+	"sync"
+	"time"
+)
 
 type Cashier interface {
-	Set(key string, value interface{})
+	Set(key string, value interface{}, dur time.Duration)
 	Get(key string) (interface{}, error)
-	Delete(key string)
+	Delete(key string) error
 }
 
-type Cashe struct {
-	storage map[string]interface{}
+type Cache struct {
 	Cashier
+	mu      *sync.RWMutex
+	storage map[string]CacheItem
 }
 
-func NewCache() *Cashe {
-	return &Cashe{storage: make(map[string]interface{})}
+type CacheItem struct {
+	value interface{}
+	ts    time.Time
+	dur   time.Duration
 }
 
-func (c *Cashe) Set(key string, value interface{}) {
-	c.storage[key] = value
+func NewCache() *Cache {
+	return &Cache{storage: make(map[string]CacheItem), mu: &sync.RWMutex{}}
 }
 
-func (c *Cashe) Get(key string) (interface{}, error) {
-	if val, ok := c.storage[key]; ok {
-		return val, nil
+func (c *Cache) Set(key string, value interface{}, dur time.Duration) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	c.storage[key] = CacheItem{
+		value: value,
+		ts:    time.Now(),
+		dur:   dur,
+	}
+}
+
+func (c *Cache) Get(key string) (interface{}, error) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	if item, ok := c.storage[key]; ok {
+		if time.Since(item.ts) < item.dur {
+			return item.value, nil
+		} else {
+			delete(c.storage, key)
+			return nil, fmt.Errorf("no data found")
+		}
 	}
 	return nil, fmt.Errorf("no data found")
 }
 
-func (c *Cashe) Delete(key string) {
-	delete(c.storage, key)
+func (c *Cache) Delete(key string) error {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	if _, ok := c.storage[key]; ok {
+		delete(c.storage, key)
+	}
+	return fmt.Errorf("no data found")
 }
